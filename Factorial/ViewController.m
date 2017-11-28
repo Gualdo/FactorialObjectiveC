9b29df80c6af2bef2309ea068e044e23e51badd6
Eduardo De La Cruz <edelacruz@teravisiontech.com>
2017-11-28 13:55:37 -0400

    All done

--- Factorial/ViewController.m
+++ Factorial/ViewController.m
@@ -14,16 +14,44 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    int miEntero = 100;
+    
+    if (miEntero > 0)
+    {
+        // Podemos calcular el factorial
+        
+        int resultado = 1;
+        
+        for (int i = 1; i <= miEntero; i++)
+        {
+            resultado = resultado * i;
+            if (resultado == 0)
+            {
+                NSLog(@"El valor  de %d es demasiado grande para ser calculado", miEntero);
+                break;
+            }
+        }
+        
+        if (resultado != 0)
+        {
+            NSLog(@"%d! = %d", miEntero, resultado);
+        }        
+    }
+    else
+    {
+        NSLog(@"Para calcular el factorial de un nuúmero, este debe ser un entero positivo");
+    }
 }
 
-
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 
 @end
